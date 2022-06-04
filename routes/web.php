683f14115f9861d8128967d5ceb47400c0b65250
Solomon APPIER-SIGN <braasig@gmail.com>
2022-06-04 03:43:59 +0000

    reorganized routes

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\InvitationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Dashboard::class);
-Route::get('/guests/check-in', [GuestController::class, 'showCheckInForm']);
-Route::put('/guests/check-in', [GuestController::class, 'checkIn']);
 Route::resource('guests', GuestController::class);
-
 Route::post('/guests/{guest}/invite', [GuestController::class, 'invite']);
 Route::resource('invitations', InvitationController::class)->only('update', 'show');
+
+Route::get('/guests/check-in', [GuestController::class, 'showCheckInForm']);
+Route::put('/guests/check-in', [GuestController::class, 'checkIn']);
