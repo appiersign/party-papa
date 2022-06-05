ec07156f6248aacd7e2f8f25cf604daec99c861b
Solomon APPIER-SIGN <braasig@gmail.com>
2022-06-05 07:38:05 +0000

    added automated removal of success alert

--- resources/js/components/Alerts.vue
+++ resources/js/components/Alerts.vue
@@ -1,18 +1,63 @@
 <template>
-    <div v-if="$page.props.error" class="card w-100 mx-auto px-2 py-3 mt-5 mb-3 bg-danger text-white text-center d-flex align-items-center">
+    <div v-if="$page.props.error"
+         class="card w-100 mx-auto px-2 py-3 mt-5 mb-3 bg-danger text-white text-center d-flex align-items-center">
         <p><b>{{ $page.props.error }}</b></p>
     </div>
-    <div v-if="$page.props.success" class="card w-100 mx-auto px-2 py-3 mt-5 mb-3 bg-success text-white text-center d-flex align-items-center">
-        <p><b>{{ $page.props.success }}</b></p>
-    </div>
+    <transition name="success" @after-enter="hideSuccess" enter-from-class="success-enter-from" enter-active-class="success-enter-active">
+        <div v-if="$page.props.success" id="success"
+             class="card w-100 mx-auto px-2 py-3 mt-5 mb-3 bg-success text-white text-center d-flex align-items-center">
+            <p><b>{{ $page.props.success }}</b></p>
+        </div>
+    </transition>
 </template>
 
 <script>
 export default {
-    name: "Alerts"
+    name: "Alerts",
+    data() {
+        return {
+            error: false,
+            success: false
+        }
+    },
+    methods: {
+        hideSuccess() {
+            setTimeout(() => {
+                this.$page.props.success = '';
+            }, 500);
+        },
+    },
 }
 </script>
 
 <style scoped>
+#success {
+    height: 74px;
+}
 
+.success-enter-from {
+    height: 0;
+}
+
+.success-enter-active {
+    height: 74px;
+    transition: height 1s ease-in-out;
+}
+
+.success-enter-to {
+    height: 74px;
+}
+
+.success-leave-from {
+    height: 74px;
+}
+
+.success-leave-active {
+    height: 0;
+    transition: height 1s ease-in-out;
+}
+
+.success-leave-to {
+    height: 0;
+}
 </style>
