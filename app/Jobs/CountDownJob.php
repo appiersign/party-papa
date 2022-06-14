fa8e7db9c324bb7ab3913badfc87f9e48ce3e704
APPIER-SIGN <chalkboardeducation@Chalkboards-MacBook-Air.local>
2022-06-14 00:00:16 +0000

    Added CountDown Notification
    - added notification via SMS
    - added count down job
    - added count down job to schedule

---
+++ app/Jobs/CountDownJob.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Guest;
+use App\Models\Invitation;
+use App\Notifications\CountDownSMSNotification;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Notification;
+
+class CountDownJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        $partyDay = Carbon::parse('2022-06-19');
+        $days = $partyDay->diffInDays(now());
+        $guests = Guest::query()
+            ->whereIn('id', Invitation::query()
+                ->where('status', '=', 'confirmed')
+                ->pluck('guest_id')
+                ->toArray())
+            ->get();
+
+        Notification::send($guests, new CountDownSMSNotification($days));
+    }
+}
---
+++ app/Notifications/CountDownSMSNotification.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class CountDownSMSNotification extends SMSNotification
+{
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(private readonly int $days)
+    {
+        parent::__construct();
+    }
+
+    public function toSMS($notifiable): string
+    {
+        if (!$this->days) {
+            $dressing = $notifiable->gender === 'female' ? ' And did you know hotness has never killed anybody? A word to the wise...' : '';
+            return "Na todaaaaaaaay! Maps link and entry code will be flying in shortly, no matter what you choose to do today, just don't die wai?{$dressing}";
+        }
+        return "Yo fam! Its {$this->days} days more to the action. Looking forward to a wicked evening together. Party starts at 1900hrs, don't be late!";
+    }
+}
